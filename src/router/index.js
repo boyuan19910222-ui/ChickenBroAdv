fd3c0fa1325ab72f13136e361deec784f42c4a0d
袁春霖 <yuanchl@ddyx.onaliyun.com>
2026-02-28 17:19:51 +0800

    fix: 修改为History模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import { useAuthStore } from '@/stores/authStore.js'
 
 const routes = [
@@ -39,7 +39,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
